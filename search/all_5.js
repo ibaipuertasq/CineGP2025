3801ac74d09c5143898c5efd9c656f2f3a6dfa80
adelafernandezdans <a.fernandezdans@opendeusto.es>
2025-05-16 18:42:54 +0000

    Deploying to gh-pages from @ ibaipuertasq/CineGP2025@bacd79daf115ed8b3d0af1931b0da1ae2b7d76aa 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['main_0',['main',['../classes_1_1deusto_1_1spq_1_1server_1_1Main.html',1,'es.deusto.spq.server.Main'],['../classes_1_1deusto_1_1spq_1_1server_1_1Main.html#acfd7bd44499e7a6462271fd5aefee074',1,'es.deusto.spq.server.Main.main()']]]
+  ['main_0',['main',['../classes_1_1deusto_1_1spq_1_1server_1_1Main.html',1,'es.deusto.spq.server.Main'],['../classes_1_1deusto_1_1spq_1_1server_1_1Main.html#acfd7bd44499e7a6462271fd5aefee074',1,'es.deusto.spq.server.Main.main()']]],
+  ['mensaje_1',['Mensaje',['../classes_1_1deusto_1_1spq_1_1server_1_1jdo_1_1Mensaje.html',1,'es::deusto::spq::server::jdo']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['main_0',['Main',['../classes_1_1deusto_1_1spq_1_1server_1_1Main.html',1,'es::deusto::spq::server']]]
+  ['main_0',['Main',['../classes_1_1deusto_1_1spq_1_1server_1_1Main.html',1,'es::deusto::spq::server']]],
+  ['mensaje_1',['Mensaje',['../classes_1_1deusto_1_1spq_1_1server_1_1jdo_1_1Mensaje.html',1,'es::deusto::spq::server::jdo']]]
 ];
