34fa2737d47628a546b3e3887760883320fd487c
ibaipuertasq <ibaipuertasq@gmail.com>
2025-05-17 12:34:18 +0000

    Deploying to gh-pages from @ ibaipuertasq/CineGP2025@cf55c00f453307acb59df558bc06b75541341fc0 🚀

--- search/functions_2.js
+++ search/functions_2.js
@@ -6,6 +6,10 @@ var searchData=
   ['getentradas_3',['getEntradas',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#aa330b6fa30b66a5dcaabe303574d076f',1,'es::deusto::spq::server::Resource']]],
   ['getpelicula_4',['getPelicula',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#a3c31be34c453bd47ab3ad699f31a56c9',1,'es::deusto::spq::server::Resource']]],
   ['getpeliculas_5',['getPeliculas',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#a5a6da276a6c2f2c1034a7d326f329ed8',1,'es::deusto::spq::server::Resource']]],
-  ['getusuario_6',['getUsuario',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#ad551a634d09a1520557ad42dcfbae8d6',1,'es::deusto::spq::server::Resource']]],
-  ['getusuarioid_7',['getUsuarioId',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#ab6138f14399408b796a8c9fc24b1fbe0',1,'es::deusto::spq::server::Resource']]]
+  ['getresumenventas_6',['getResumenVentas',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#aa05c3727ac57772355e6c9b37eb21e9a',1,'es::deusto::spq::server::Resource']]],
+  ['getusuario_7',['getUsuario',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#ad551a634d09a1520557ad42dcfbae8d6',1,'es::deusto::spq::server::Resource']]],
+  ['getusuarioid_8',['getUsuarioId',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#ab6138f14399408b796a8c9fc24b1fbe0',1,'es::deusto::spq::server::Resource']]],
+  ['getventaspordia_9',['getVentasPorDia',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#ad67bc60fdbb38b10fa6ea43888324f7c',1,'es::deusto::spq::server::Resource']]],
+  ['getventasporpelicula_10',['getVentasPorPelicula',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#a4a1f649227b1b9f031d70fa9022ce82d',1,'es::deusto::spq::server::Resource']]],
+  ['getventasportipoasiento_11',['getVentasPorTipoAsiento',['../classes_1_1deusto_1_1spq_1_1server_1_1Resource.html#a555d417e57bca984df4058e629f64487',1,'es::deusto::spq::server::Resource']]]
 ];
