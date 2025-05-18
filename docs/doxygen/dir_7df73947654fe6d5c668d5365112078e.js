1e1d3a37827e7efa1d76502f55eb7be3d4d5e623
adelafernandezdans <a.fernandezdans@opendeusto.es>
2025-05-18 18:06:42 +0200

    cambios maven.yml

--- docs/doxygen/dir_7df73947654fe6d5c668d5365112078e.js
+++
@@ -1,4 +0,0 @@
-var dir_7df73947654fe6d5c668d5365112078e =
-[
-    [ "server", "dir_de437d1c073fb9332ce6284ff6cf50be.html", "dir_de437d1c073fb9332ce6284ff6cf50be" ]
-];
--- docs/doxygen/dir_de437d1c073fb9332ce6284ff6cf50be.js
+++
@@ -1,5 +0,0 @@
-var dir_de437d1c073fb9332ce6284ff6cf50be =
-[
-    [ "Main.java", "Main_8java_source.html", null ],
-    [ "Resource.java", "Resource_8java_source.html", null ]
-];
